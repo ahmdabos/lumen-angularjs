778a84a74b870ee74ad3ce0ab7f8d0b1f8857355
ahmdabos <ahmdabos@gmail.com>
2017-10-06 21:00:00 +0400

    install lumen + configuration + create api + apply oauth2

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,19 @@
+<?php namespace App\Http\Controllers;
+use App\Course;
+class CourseController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::all();
+        return $this->createSuccessResponse($courses, 200);
+    }
+    public function show($id)
+    {
+        $course = Course::find($id);
+        if($course)
+        {
+            return $this->createSuccessResponse($course, 200);
+        }
+        return $this->createErrorResponse("The course whith id {$id}, does not exists", 404);
+    }
+}
